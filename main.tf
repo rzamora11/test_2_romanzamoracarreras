946b88500ff38920df5d50d8bd9cf8783b49e0c1
rzamora11 <71715749+rzamora11@users.noreply.github.com>
2024-03-09 20:20:18 +0100

    Changed count to 5

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-/*resource "null_resource" "tfc_test_workingfolder" {
-  provisioner "local-exec" {
-    command = "echo 'Working Folder Test!!'"
-  }
-}*/
-
 resource "null_resource" "tfc_test" {
-  count = 10
+  count = 5
   provisioner "local-exec" {
     command = "echo 'Test ${count.index}'"
   }
